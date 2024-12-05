cc73d0d19d45e32bc723e991f76cb50557146cbf
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-05 19:40:54 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div id="app">
-            <example-component></example-component>
+            <app-component></app-component>
         </div>
     </body>
 </html>
