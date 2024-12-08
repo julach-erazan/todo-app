65a992f4d864627cceb8034e3b72fc828866bf2d
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-08 21:17:28 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,23 +26,4 @@ class UserController extends BaseController
 
         return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
     }
-
-    // Get all users
-    public function index()
-    {
-        $user = User::all();
-        return response()->json($user, 200);
-    }
-
-    // Get user by ID
-    public function show($id)
-    {
-        $user = User::find($id);
-
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-
-        return response()->json($user, 200);
-    }
 }
