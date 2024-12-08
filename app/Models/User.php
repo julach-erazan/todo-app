83ba728bf3df1eccc8e4c597f10cf780c1c6ad5d
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-08 20:08:01 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     protected $fillable = ['name', 'email', 'password'];
 
-    // Define relationship with tasks
     public function tasks()
     {
         return $this->hasMany(Task::class);
