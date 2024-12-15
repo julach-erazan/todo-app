a505c15b4e202ff93d3c65849bf3395992b251f0
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-16 04:50:44 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use HasFactory;
     use HasApiTokens, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'image'];
 
     protected $hidden = [
         'password',
