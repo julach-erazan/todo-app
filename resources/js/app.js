7ec776baf8b04f793f27daf6356e1cebb4d51552
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-16 04:50:18 +0530

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
-import './bootstrap';
 import { createApp } from 'vue';
 import router from "./router";
-
+import FontAwesomeIcon from './fontawesome.js';
 import AppComponent from './src/App.vue';
-createApp(AppComponent).use(router).mount("#app");
+
+const app = createApp(AppComponent);
+
+app.use(router).mount("#app");
+app.component('font-awesome-icon', FontAwesomeIcon);
