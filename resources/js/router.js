4268614e321e1727f30482e0c70de660032d8f05
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-16 04:49:41 +0530

    Update router.js

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../js/src/components/Login.vue";
 import Register from "../js/src/components/Register.vue";
 import Dashboard from "../js/src/components/Dashboard.vue";
+import ViewPost from "../js/src/components/ViewPost.vue";
+import AddPost from "../js/src/components/AddPost.vue";
+import UpdatePost from "../js/src/components/UpdatePost.vue";
 
 const routes = [
   { path: "/login", name: "Login", component: Login },
@@ -13,20 +16,19 @@ const routes = [
     meta: { requiresAuth: true },
   },
   { path: "/", redirect: "/login" },
+  { path: "/viewpost", name: "viewPost", component: ViewPost, },
+  { path: "/addpost", name: "addPost", component: AddPost, },
+  { 
+    path: "/updatepost/:id", 
+    name: "updatePost",
+    component: UpdatePost ,
+    props: true,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
-
-router.beforeEach((to, from, next) => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-  if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
-    next("/login");
-  } else {
-    next();
-  }
 });
 
 export default router;
